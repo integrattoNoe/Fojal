0f6f2e4906dbaa629e7aaa2a3faa72100e9fb224
nefas1809 <nefas1809@gmail.com>
2018-10-23 12:15:11 -0500

    fojal_0.1.2.1
    /*AÑADIDO A 0.1.2*/
    *gitIgnore Uploads
    *rutas actualizadas

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'login';
 $route["login"] = "login/login";
-$route["emprendimiento_social"] = "programas_y_tramites/emprendimientoSocial";
+$route["emprendimiento_social"] = "programas_y_tramites/EmprendimientoSocial";
+$route["emprendimiento_tradicional"] = "programas_y_tramites/EmprendimientoTradicional";
+$route["emprendimiento_institucional"] = "programas_y_tramites/EmprendimientoInstitucional";
+$route["emprendimiento_alto_impacto"] = "programas_y_tramites/EmprendimientoAltoImpacto";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
